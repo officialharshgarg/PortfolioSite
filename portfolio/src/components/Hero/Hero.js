8e5a70fce7db62930702eef0ef4ecd6b21cdcb32
HARSH GARG <officialharshgarg>
2024-07-25 17:31:49 +0530

    project section commit

--- portfolio/src/components/Hero/Hero.js
+++ portfolio/src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        Here I am gonna give you brief intro to my education and carrer
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn more
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
+/* const projectsDemo = [
+  { title: "Project 1", description: "This is description" },
+  { title: "Project 2", description: "This is description" },
+  { title: "Project 3", description: "This is description" },
+  { title: "Project 4", description: "This is description" },
+];
+ */
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <br />
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <br />
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <br />
+            <UtilityList>
+              <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
